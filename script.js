5e1463bbb93188e3d0ebab677bb6faea4cc7d9b1
gelso <gelso02@live.com>
2022-05-05 17:59:30 -0300

    feito requisito 3

---
+++ script.js
@@ -0,0 +1,17 @@
+//03)
+function login() {
+    const botao = document.getElementById('submit')
+    const inputEmail = document.getElementById('email')
+    const inputSenha = document.getElementById('senha')
+    const email = 'tryber@teste.com'
+    botao.addEventListener('click', function () {
+        //      console.log('tipo input email: ',typeof inputEmail.value);
+        //     console.log('email: ',inputEmail.value);
+        //     console.log('tipo input senha: ',typeof inputSenha.value);
+        //    console.log('senha: ',parseInt(inputSenha.value));
+        if (inputEmail.value == email && inputSenha.value == 123456) {
+            alert('Olá, Tryber!')
+        } else alert('Email ou senha inválidos.')
+    })
+}
+login()
