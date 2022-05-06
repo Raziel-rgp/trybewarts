991abe7013f7fcc8f0af4fc933649455f9f64695
gelso <gelso02@live.com>
2022-05-05 21:15:02 -0300

    ./node_modules/.bin/eslint . --fix

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function login() {
   const inputEmail = document.getElementById('email');
   const inputSenha = document.getElementById('senha');
   const email = 'tryber@teste.com';
-  botao.addEventListener('click', function () {
+  botao.addEventListener('click', () => {
   // console.log('tipo input email: ',typeof inputEmail.value);
   // console.log('email: ',inputEmail.value);
   // console.log('tipo input senha: ',typeof inputSenha.value);
   // console.log('senha: ',parseInt(inputSenha.value));
-    if (inputEmail.value == email && inputSenha.value == 123456) {
+    if (inputEmail.value === email && inputSenha.value === '123456') {
       alert('Olá, Tryber!');
     } else alert('Email ou senha inválidos.');
-  },);  
+  });
 }
 login();
