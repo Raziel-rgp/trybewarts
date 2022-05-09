25d21ff48ee1ff537f8d0b3455aab5f113d0c49e
gelso <gelso02@live.com>
2022-05-09 00:17:05 -0300

    fim do projeto, restando apenas 5 linhas para corrigir no lint

--- script.js
+++ script.js
@@ -16,21 +16,26 @@ function login() {
 }
 login();
 
+// 18)
 function check() {
   const botao = document.getElementById('submit-btn');
   const checker = document.getElementById('agreement');
   botao.disabled = true;
-  botao.style.backgroundColor = 'rgb(182, 181, 181)';
-  botao.style.cursor = 'progress';
   checker.addEventListener('click', () => {
-    // alert('checado')
-    botao.disabled = false;
-    botao.style.backgroundColor = 'darkslateblue';
-    botao.style.cursor = 'pointer';
+    if (checker.checked === true) {
+      botao.disabled = false;
+      botao.style.backgroundColor = 'darkslateblue';
+      botao.style.cursor = 'pointer';
+    } else {
+      botao.disabled = true;
+      botao.style.backgroundColor = 'rgb(182, 181, 181)';
+      botao.style.cursor = 'not-allowed';
+    }
   });
 }
 check();
 
+// 20)
 function contador() {
   const text = document.getElementsByTagName('textarea')[0];
   const cont = document.getElementById('counter');
@@ -42,3 +47,51 @@ function contador() {
   });
 }
 contador();
+
+// 21)
+// https://developer.mozilla.org/pt-BR/docs/Web/API/Node/removeChild
+const nome = document.getElementById('input-name');
+const sobrenome = document.getElementById('input-lastname');
+const email = document.getElementById('input-email');
+const casa = document.getElementById('house');
+const familia = document.querySelectorAll('#input');
+let valor = '';
+const materias = document.querySelectorAll('.subject');
+const valorMaterias = [];
+const avaliacao = document.querySelectorAll('.input');
+let nota = '';
+const obs = document.getElementById('textarea');
+const botao = document.getElementById('submit-btn');
+const form = document.getElementById('evaluation-form');
+
+function exibir() {
+  botao.addEventListener('click', () => {
+    for (let index = 0; index < familia.length; index += 1) {
+      if (familia[index].checked === true) {
+        valor = familia[index].value;
+      }
+    }
+    for (let index = 0; index < materias.length; index += 1) {
+      if (materias[index].checked === true) {
+        valorMaterias.push(` ${materias[index].value}`);
+      }
+    }
+    for (let index = 0; index < avaliacao.length; index += 1) {
+      if (avaliacao[index].checked === true) {
+        nota = avaliacao[index].value;
+      }
+    }
+    while (form.firstChild) { form.removeChild(form.firstChild); }
+    const p = document.createElement('p');
+    p.innerText = `Nome: ${nome.value} ${sobrenome.value}\n Email: ${email.value}\n Casa: ${casa.value}\n Família: ${valor} \n Matérias: ${valorMaterias} \n Avaliação: ${nota}\n Observações: ${obs.value}`;
+    form.append(p);
+  });
+}
+exibir();
+
+// function remover() {
+//   const form = document.getElementById('evaluation-form')
+//   // if(form.parentNode){
+//   form.parentNode.removeChild(form)
+//   // }
+// }
